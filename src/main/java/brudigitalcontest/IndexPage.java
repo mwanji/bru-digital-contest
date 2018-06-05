2911254891638c3d2fbbebd74012c90ced3e3a4e
Moandji Ezana <moandji.ezana@brutex.com>
2018-06-05 13:32:58 +0200

    Use GridPage in IndexPage

--- src/main/java/brudigitalcontest/IndexPage.java
+++ src/main/java/brudigitalcontest/IndexPage.java
@@ -1,5 +1,6 @@
 package brudigitalcontest;
 
+import brudigitalcontest.html.GridPage;
 import lombok.AllArgsConstructor;
 
 import static j2html.TagCreator.*;
@@ -10,34 +11,24 @@ public class IndexPage {
   private final Texts texts;
 
   public String render() {
-    return document(html(attrs(".splash"),
-      head(
-        meta().withCharset("utf-8"),
-        meta().attr("name", "viewport").attr("content", "width=device-width, initial-scale=1, shrink-to-fit=no"),
-        link().withRel("stylesheet").withHref("https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css").attr("integrity", "sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB").attr("crossorigin", "anonymous"),
-//        link().withRel("stylesheet").withHref("https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css").attr("integrity", "sha256-BJ/G+e+y7bQdrYkS2RBTyNfBHpA9IuGaPmf9htub5MQ=").attr("crossorigin", "anonymous"),
-        link().withRel("stylesheet").withHref("/application.css"),
-        title("Contest - bru.digital")
-      ),
-      body(attrs("#bruDigitalContest.splash"),
-        div(attrs(".splash"),
-          div(attrs(".container-fluid"),
-            div(attrs(".row"),
-              div(attrs(".splashContainer.col-8.m-auto.align-middle"),
-                div(attrs(".splashTitleBox.text-center.w-100.p-5.mb-5"),
-                  h1(attrs(".display-3.bru-text-light.p-4"), texts.welcome())
-                ),
-                p(attrs(".m-5"), rawHtml("&nbsp;")),
-                label(attrs(".text-white.mt-5"), h3(attrs(".bru-semi-bold"), texts.startNameLabel())),
-                form(attrs(".form-inline"),
-                  input(attrs(".bru-bg-light.bru-height-high.form-control.form-control-lg.col-8")).withType("text").withName("name").isRequired(),
-                  button(attrs(".btn.bg-white.bru-height-high.bru-text-light.col-4"), h2(attrs(".bru-bold"), texts.startSubmitLabel())).withType("submit")
-                ).withAction("/contest").withMethod("post")
-              )
+    return new GridPage().render(
+      div(attrs(".bru-index-bg.h-100"),
+        div(attrs(".container-fluid.h-100"),
+          div(attrs(".row.h-100"),
+            div(attrs(".d-inline-block.col-8.m-auto.align-middle"),
+              div(attrs(".splashTitleBox.text-center.w-100.p-5.mb-5"),
+                h1(attrs(".display-3.bru-text-light.p-4.bru-bold"), texts.welcome())
+              ),
+              p(attrs(".m-5"), rawHtml("&nbsp;")),
+              label(attrs(".text-white.mt-5"), h3(attrs(".bru-semi-bold"), texts.startNameLabel())),
+              form(attrs(".form-inline"),
+                input(attrs(".bru-bg-light.bru-height-high.bru-square.form-control.form-control-lg.col-8")).withType("text").withName("name").isRequired(),
+                button(attrs(".btn.bg-white.bru-height-high.bru-text-light.bru-square.col-4"), h2(attrs(".bru-bold"), texts.startSubmitLabel())).withType("submit")
+              ).withAction("/contest").withMethod("post")
             )
           )
         )
       )
-    ));
+    );
   }
 }
---
+++ src/main/java/brudigitalcontest/html/GridPage.java
@@ -0,0 +1,29 @@
+package brudigitalcontest.html;
+
+import j2html.tags.DomContent;
+
+import static j2html.TagCreator.*;
+
+public class GridPage {
+
+  public String render(DomContent... contents) {
+    return document(html(attrs(".h-100.m-0"),
+      head(
+        meta().withCharset("utf-8"),
+        meta().attr("name", "viewport").attr("content", "width=device-width, initial-scale=1, shrink-to-fit=no"),
+        link().withRel("stylesheet").withHref("https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css").attr("integrity", "sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB").attr("crossorigin", "anonymous"),
+//        link().withRel("stylesheet").withHref("https://cdnjs.cloudflare.com/ajax/libs/open-iconic/1.1.1/font/css/open-iconic-bootstrap.min.css").attr("integrity", "sha256-BJ/G+e+y7bQdrYkS2RBTyNfBHpA9IuGaPmf9htub5MQ=").attr("crossorigin", "anonymous"),
+        link().withRel("stylesheet").withHref("/application.css"),
+        title("Contest - bru.digital")
+      ),
+      body(attrs("#bruDigitalContest.h-100.m-0"),
+        div(attrs(".h-100"), contents),
+        script().withSrc("https://code.jquery.com/jquery-3.3.1.slim.min.js").attr("integrity", "sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo").attr("crossorigin", "anonymous"),
+        script().withSrc("https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js").attr("integrity", "sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49").attr("crossorigin", "anonymous"),
+        script().withSrc("https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js").attr("integrity", "sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T").attr("crossorigin", "anonymous"),
+        script().withSrc("/application.js")
+      )
+    ));
+
+  }
+}
